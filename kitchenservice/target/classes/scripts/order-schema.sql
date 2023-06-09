64da83d8f49ed5f6f9abd35bbcb4194df831e188
Sbaker2196 <sean.b@hotmail.de>
2023-06-09 10:18:30 +0200

    Command Side Implementation Done

---
+++ kitchenservice/target/classes/scripts/order-schema.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE kitchen_order_schema;
+USE kitchen_order_schema;
+CREATE TABLE orders(
+    orderID INT,
+    name VARCHAR(255),
+    price VARCHAR(255)
+);
---
+++ kitchenservice/target/classes/scripts/recipe-schema-load-data.sql
@@ -0,0 +1,12 @@
+INSERT INTO kitchenRecipes(name, gramsWater, gramsSugar, gramsButter, numOfSalamiSlices, gramsCreamCheese)
+VALUES("Croissant", 200, NULL, 200, NULL, NULL);
+
+INSERT INTO kitchenRecipes(name, gramsWater, gramsSugar, gramsButter, numOfSalamiSlices, gramsCreamCheese)
+VALUES("Bagel", 200, NULL, 50, NULL, NULL);
+
+INSERT INTO kitchenRecipes(name, gramsWater, gramsSugar, gramsButter, numOfSalamiSlices, gramsCreamCheese)
+VALUES("Cheesecake", 200, 300, 200, NULL, 350);
+
+INSERT INTO kitchenRecipes(name, gramsWater, gramsSugar, gramsButter, numOfSalamiSlices, gramsCreamCheese)
+VALUES("Sandwich", NULL, NULL, 20, 3, 50);
+
